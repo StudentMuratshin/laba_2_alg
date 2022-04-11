03ee2a5e66614bd71371836072f42fb7b3e5461d
Ильгам Муратшин <muratshin.ilgam96@gmail.com>
2022-04-11 11:25:26 +0500

    asd

--- generator.h
+++ generator.h
@@ -13,11 +13,13 @@ public:
 	}
 };
 
+
 class def {
 	int n, w, max = 0;
+	int k = 0;
 	vector<Pointt> ps;
 	vector<Pointt> res;
-	vector<Pointt> Max_par{ Pointt(0,0,"z") };
+	vector<Pointt> Max_par{ Pointt(0,0,"b") };
 public:
 	void Read(string fname) {
 		ifstream f;
@@ -27,12 +29,12 @@ public:
 		int x, y;
 		for (int i = 0; i < n; i++) {
 			f >> x >> y;
-			if (x > max && x <= w)
+			if (y > max && x <= w)
 			{
 				Max_par[0].x = x;
 				Max_par[0].y = y;
 				Max_par[0].s = "P" + std::to_string(i + 1);
-				max = x;
+				max = y;
 			}
 			ps.emplace_back(x, y, "P" + std::to_string(i + 1));
 		}
@@ -43,13 +45,6 @@ public:
 	{
 		for (int i = 0; i < n; i++)
 		{
-			if (Wes <=w && Wes >= max)
-			{
-				Max_par[0].x = Wes;
-				Max_par[0].y = Pri;
-				Max_par[0].s = Name;
-				max = Wes;
-			}
 			Wes += ps[i].x;
 			if (Wes > w) { Wes -= ps[i].x; continue; }
 			Pri += ps[i].y;
@@ -57,8 +52,7 @@ public:
 			Name += ps[i].s + "+";
 			if (Wes <= w)
 			{
-				res.emplace_back(Wes, Pri, Name); 
-				//cout << Name << ":    " << Wes << "       " << Pri << endl;
+				res.emplace_back(Wes, Pri, Name);
 				Add(Wes, Pri, Name);
 				Wes -= ps[i].x;
 				Pri -= ps[i].y;
@@ -67,14 +61,41 @@ public:
 		}
 	}
 
-	void Print() {
+	void Sort_max()
+	{
+		for (Pointt p : res)
+		{
+			if (p.y >= max)
+			{
+				Max_par[0].x = p.x;
+				Max_par[0].y = p.y;
+				Max_par[0].s = p.s;
+				max = p.y;
+			}
+		}
+	}
+
+	void Sort_row()
+	{
+		for (int i = 0; i < res.size(); i++)
+		{
+			for (int j = i + 1; j < res.size(); j++)
+			{
+				if ((res[i].s).size() == (res[j].s).size() && )
+			}
+		}
+	}
+
+	void Print() 
+	{
 		for (Pointt& p : ps) {
 			cout << p.s << ":       " << p.x << "       " << p.y << endl << endl;
 		}
 		for (int i = 0; i < res.size(); i++) {
 			cout << res[i].s << ":    " << res[i].x << "       " << res[i].y << endl;
 		}
-		cout << endl;
+		cout << endl; 
+		Sort_max();
 		cout << Max_par[0].s << ":       " << Max_par[0].x << "       " << Max_par[0].y << "      <------ Best option" << endl;
 	}
 
